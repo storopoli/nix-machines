d3b4973fff6227ebafd0ffeb6c830dd3344420bb
Jose Storopoli <jose@storopoli.com>
2025-07-26 12:14:58 -0300

    feat(desktop): GNOME settings

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -29,6 +29,22 @@
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
   };
 
+  dconf = {
+    enable = true;
+    settings = {
+      # GNOME Dark mode
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+      # Better video display
+      "org/gnome/mutter" = {
+        experimental-features = [
+          "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
+          "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
+          "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+        ];
+      };
+    };
+  };
+
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
