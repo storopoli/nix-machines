4dd8a70c45879a4883531f3cd0d4851c29a7bd54
Jose Storopoli <jose@storopoli.com>
2025-07-31 07:12:58 -0300

    feat(helix): add lua-language-server

--- home-manager/helix.nix
+++ home-manager/helix.nix
@@ -21,6 +21,7 @@
     taplo
     tinymist
     yaml-language-server
+    lua-language-server
 
     # Formatter
     nixfmt-rfc-style
@@ -211,7 +212,28 @@
             };
           };
         };
+
+        lua-language-server.config = {
+          Lua = {
+            # Tell the language server which version of Lua you're using
+            # (most likely LuaJIT in the case of Neovim)
+            runtime.version = "LuaJIT";
+
+            # Get the language server to recognize the `vim` global
+            diagnostics.globals = [
+              "vim"
+              "require"
+            ];
+
+            # Make the server aware of Neovim runtime files
+            workspace.library = ''vim.api.nvim_get_runtime_file(""; true)'';
+
+            # Do not send telemetry data containing a randomized but unique identifier
+            telemetry.enable = false;
+          };
+        };
       };
+
       language = [
         {
           name = "nix";
@@ -272,6 +294,10 @@
           auto-format = true;
         }
 
+        {
+          name = "lua";
+          auto-format = true;
+        }
       ];
     };
   };
