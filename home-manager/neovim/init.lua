1fae9742edc97639315a106e6566addd6cd815b2
Jose Storopoli <jose@storopoli.com>
2025-08-04 19:34:21 -0300

    fix(neovim): wrong yamlls schemas

--- home-manager/neovim/init.lua
+++ home-manager/neovim/init.lua
@@ -549,10 +549,10 @@ vim.lsp.config("yamlls", {
       validation = true,
       schemas = {
         ["kubernetes"] = "*.yaml",
-        ["https=//raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
+        ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] =
         "docker-compose.yaml",
         ["https://json.schemastore.org/github-workflow.json"] = ".github/workflows/*",
-        ["https=//json.schemastore.org/github-action.json"] = ".github/actions/*/action.yaml",
+        ["https://json.schemastore.org/github-action.json"] = ".github/actions/*/action.yaml",
       },
     },
   }
