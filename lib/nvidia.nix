f4537f8b4ef3b2512575dff7cdcf23acb2085d2f
Jose Storopoli <jose@storopoli.com>
2025-08-18 14:02:15 -0300

    feat: nvidia tweaks

--- lib/nvidia.nix
+++ lib/nvidia.nix
@@ -6,27 +6,41 @@
 
 {
   # Nvidia Configuration
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        vdpauinfo # sudo vainfo
+        libva-utils # sudo vainfo
+        # https://discourse.nixos.org/t/nvidia-open-breaks-hardware-acceleration/58770/2
+        nvidia-vaapi-driver
+        vaapiVdpau
+        nvtopPackages.nvidia
+      ];
+    };
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = true;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  environment.variables = {
+    NVD_BACKEND = "direct";
+    LIBVA_DRIVER_NAME = "nvidia";
   };
+  boot.blacklistedKernelModules = [
+    "nouveau"
+    "i915"
+  ];
 
   # CUDA
   environment.systemPackages = with pkgs; [
     cudaPackages.cudatoolkit
     cudaPackages.cudnn
   ];
-
-  environment.variables = {
-    CUDA_PATH = "${pkgs.cudatoolkit}";
-  };
+  environment.variables.CUDA_PATH = "${pkgs.cudatoolkit}";
 }
