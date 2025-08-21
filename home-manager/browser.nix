3849c2be1657623110b01faec5885374c64ebd60
Jose Storopoli <jose@storopoli.com>
2025-08-21 06:56:04 -0300

    fix(browser): use brave

--- home-manager/browser.nix
+++ home-manager/browser.nix
@@ -11,6 +11,9 @@ in
 {
   programs.chromium = lib.mkIf isLinux {
     enable = true;
+
+    package = pkgs.brave;
+
     commandLineArgs = [ "--ozone-platform=wayland" ];
 
     dictionaries = with pkgs; [
