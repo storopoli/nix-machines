f9312077f7dbe45d1f348b04c11dc3d493ad0663
Jose Storopoli <jose@storopoli.com>
2025-09-01 09:05:09 -0300

    fix(git): deployment

--- hosts/git/hardware-configuration.nix
+++ hosts/git/hardware-configuration.nix
@@ -14,21 +14,17 @@
     "ata_piix"
     "uhci_hcd"
     "virtio_pci"
+    "virtio_blk"
     "virtio_scsi"
     "sd_mod"
     "sr_mod"
   ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
 
   # Bootloader
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-
-  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -38,4 +34,5 @@
   # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  virtualisation.hypervGuest.enable = lib.mkDefault true;
 }
