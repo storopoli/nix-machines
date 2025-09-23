abc1f0b97b2ec12cf9eae3db9b485d9f2611632a
Jose Storopoli <jose@storopoli.com>
2025-09-23 12:19:03 -0300

    fix(gnome): appindicator tray

--- home-manager/linux/gnome.nix
+++ home-manager/linux/gnome.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{
+  pkgs,
+  ...
+}:
 
 {
+  home.packages = with pkgs.gnomeExtensions; [
+    blur-my-shell
+    gsconnect
+    appindicator
+  ];
   dconf = {
     enable = true;
     settings = {
@@ -12,6 +20,14 @@
           "scale-monitor-framebuffer" # Enables fractional scaling (125% 150% 175%)
           "variable-refresh-rate" # Enables Variable Refresh Rate (VRR) on compatible displays
           "xwayland-native-scaling" # Scales Xwayland applications to look crisp on HiDPI screens
+        ];
+      };
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          gsconnect.extensionUuid
+          appindicator.extensionUuid
         ];
       };
     };
