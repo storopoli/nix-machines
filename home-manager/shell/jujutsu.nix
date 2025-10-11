6ce9f088f89bad0e708cdb52efe2dc9dc02ce973
Jose Storopoli <jose@storopoli.com>
2025-10-11 13:34:27 -0300

    feat(jujutsu): add SSH signing

--- home-manager/shell/jujutsu.nix
+++ home-manager/shell/jujutsu.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{
+  secretiveFingerprint,
+  ...
+}:
 
 {
-  programs.jjui.enable = true;
   programs.jujutsu = {
     enable = true;
     settings = {
@@ -9,10 +11,26 @@
         name = "Jose Storopoli";
         email = "jose@storopoli.com";
       };
+
       ui = {
         default-command = "log";
         diff-editor = "nvim -d";
       };
+
+      signing = {
+        behavior = "own";
+        backend = "ssh";
+        key = secretiveFingerprint;
+        backends = {
+          allowed-signers = "~/.ssh/allowed_signers";
+        };
+      };
+
+      git = {
+        sign-on-push = true;
+      };
     };
   };
+
+  programs.jjui.enable = true;
 }
