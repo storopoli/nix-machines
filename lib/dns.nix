6e85ec0b9576f09c3b7ee33add9c604dff2ee799
Jose Storopoli <jose@storopoli.com>
2026-06-22 12:27:20 -0300

    chore(dns): fix deprecated options

--- lib/dns.nix
+++ lib/dns.nix
@@ -8,14 +8,16 @@
   ];
   services.resolved = {
     enable = true;
-    dnssec = "true";
-    domains = [ "~." ];
-    fallbackDns = [
-      "1.1.1.1"
-      "1.0.0.1"
-      "8.8.8.8"
-      "8.8.4.4"
-    ];
-    dnsovertls = "true";
+    settings.Resolve = {
+      DNSSEC = "true";
+      Domains = [ "~." ];
+      FallbackDNS = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "8.8.8.8"
+        "8.8.4.4"
+      ];
+      DNSOverTLS = "true";
+    };
   };
 }
